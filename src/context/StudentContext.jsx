c595a7d5430bdc62f33bf490c641557cee5d3dc4
Shashwat Gupta <shashwatsatna@gmail.com>
2020-12-20 04:27:37 +0530

    logout

--- src/context/StudentContext.jsx
+++ src/context/StudentContext.jsx
@@ -1,9 +1,9 @@
 import React, {
-	createContext,
-	useContext,
-	useEffect,
-	useReducer,
-	useState,
+  createContext,
+  useContext,
+  useEffect,
+  useReducer,
+  useState,
 } from "react";
 import { fetchStudentProfile } from "../API/GET";
 import Loading from "../pages/Loading";
@@ -11,84 +11,82 @@ import Loading from "../pages/Loading";
 export const StudentContext = createContext();
 
 const initialState = {
-	isLoggedIn: false,
-	studentProfile: {},
+  isLoggedIn: false,
+  studentProfile: {},
 };
 
 const StudentContextProvider = ({ children }) => {
-	const [state, dispatch] = useReducer(studentReducer, initialState);
+  const [state, dispatch] = useReducer(studentReducer, initialState);
 
-	const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(true);
 
-	useEffect(() => {
-		const token = localStorage.getItem("studentAuthToken");
+  useEffect(() => {
+    const token = localStorage.getItem("studentAuthToken");
 
-		if (token) {
-			setLoginTrue(token);
-		} else {
-			setLoading(false);
-		}
-	}, []);
+    if (token) {
+      setLoginTrue(token);
+    } else {
+      setLoading(false);
+    }
+  }, []);
 
-	const setLoginTrue = async (token) => {
-		const profile = await fetchStudentProfile(token);
+  const setLoginTrue = async (token) => {
+    const profile = await fetchStudentProfile(token);
 
-		if (profile) {
-			dispatch({ type: "SET_LOGIN_TRUE" });
-			setStudentDetails(profile);
-			localStorage.setItem("studentAuthToken", token);
-		} else {
-			localStorage.clear();
-		}
+    if (profile) {
+      dispatch({ type: "SET_LOGIN_TRUE" });
+      setStudentDetails(profile);
+      localStorage.setItem("studentAuthToken", token);
+    } else {
+      localStorage.clear();
+    }
 
-		setLoading(false);
-	};
+    setLoading(false);
+  };
 
-	const setLoginFalse = () => {
-		localStorage.clear();
-		dispatch({ type: "SET_LOGIN_FALSE" });
-	};
+  const setLoginFalse = () => {
+    localStorage.clear();
+    dispatch({ type: "SET_LOGIN_FALSE" });
+  };
 
-	const setStudentDetails = (profile) => {
-		dispatch({ type: "SET_STUDENT_PROFILE", payload: profile });
-	};
+  const setStudentDetails = (profile) => {
+    dispatch({ type: "SET_STUDENT_PROFILE", payload: profile });
+  };
 
-	const value = {
-		...state,
-		setLoginTrue,
-		setLoginFalse,
-		setStudentDetails,
-	};
+  const value = {
+    ...state,
+    setLoginTrue,
+    setLoginFalse,
+    setStudentDetails,
+  };
 
-	if (loading) {
-		return <Loading />;
-	}
+  if (loading) {
+    return <Loading />;
+  }
 
-	return (
-		<StudentContext.Provider value={value}>
-			{children}
-		</StudentContext.Provider>
-	);
+  return (
+    <StudentContext.Provider value={value}>{children}</StudentContext.Provider>
+  );
 };
 
 export default StudentContextProvider;
 
 const studentReducer = (state, action) => {
-	switch (action.type) {
-		case "SET_LOGIN_TRUE":
-			return {
-				...state,
-				isLoggedIn: true,
-			};
-		case "SET_LOGIN_FALSE":
-			return {
-				...state,
-				isLoggedIn: false,
-			};
-		case "SET_STUDENT_PROFILE":
-			return {
-				...state,
-				studentProfile: action.payload,
-			};
-	}
+  switch (action.type) {
+    case "SET_LOGIN_TRUE":
+      return {
+        ...state,
+        isLoggedIn: true,
+      };
+    case "SET_LOGIN_FALSE":
+      return {
+        ...state,
+        isLoggedIn: false,
+      };
+    case "SET_STUDENT_PROFILE":
+      return {
+        ...state,
+        studentProfile: action.payload,
+      };
+  }
 };
--- src/pages/Student/StudProfile/StudProfile.js
+++ src/pages/Student/StudProfile/StudProfile.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect, useContext } from "react";
 import axios from "axios";
+import NavbarStud from "../../../hoc/NavbarStud/NavbarStud";
+
 import {
   Paper,
-  Avatar,
   Container,
   Button,
   Grid,
@@ -16,11 +17,7 @@ import {
   IconButton,
 } from "@material-ui/core";
 import { StudentContext } from "../../../context/StudentContext";
-import { Alert } from "@material-ui/lab";
-import { patchProfile } from "../../../API/PATCH";
-import { AccountCircle, AspectRatio } from "@material-ui/icons";
-import UpdateProfilePhoto from "../../../components/Club/ProfileModals/UpdateProfilePhoto";
-import UpdateBannerPhoto from "../../../components/Club/ProfileModals/UpdateBannerPhoto";
+import { patchStudProfile } from "../../../API/PATCH";
 
 const useStyles = makeStyles((theme) => ({
   avatar: {
@@ -49,215 +46,135 @@ const ClubProfile = () => {
 
   const [disabled, setDisabled] = useState(true);
 
-  const [profileSuccess, setProfileSuccess] = useState(false);
+  const handleProfileChange = (e) => {
+    setStudData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
 
-  const [dpModal, setDpModal] = useState(false);
-  const [bannerModal, setBannerModal] = useState(false);
+  const updateProfile = async () => {
+    setDisabled(true);
+    const token = localStorage.getItem("clubAuthToken");
 
-  // const handleProfileChange = (e) => {
-  //   setData((prevState) => ({
-  //     ...prevState,
-  //     [e.target.name]: e.target.value,
-  //   }));
-  // };
+    const res = await patchStudProfile(studData, token);
 
-  // const updateProfile = async () => {
-  //   setDisabled(true);
-  //   const token = localStorage.getItem("clubAuthToken");
+    // if (res) {
+    //   getProfile(token);
+    //   setProfileSuccess(true);
+    // }
 
-  //   const res = await patchProfile(data, token);
+    setDisabled(false);
+  };
 
-  //   if (res) {
-  //     getProfile(token);
-  //     setProfileSuccess(true);
-  //   }
-
-  //   setDisabled(false);
-  // };
-
-  // const updateImages = () => {
-  //   setData((prevState) => ({
-  //     ...prevState,
-  //     clubAvatar: clubDetails.club.clubAvatar,
-  //     clubBanner: clubDetails.club.clubBanner,
-  //   }));
-  //   console.log(data, clubDetails.club);
-  // };
-
-  // useEffect(() => {
-  //   if (JSON.stringify(data) === JSON.stringify(clubDetails.club)) {
-  //     setDisabled(true);
-  //   } else {
-  //     setDisabled(false);
-  //   }
-  // }, [data, clubDetails]);
+  useEffect(() => {
+    if (JSON.stringify(studData) === JSON.stringify(studentProfile.club)) {
+      setDisabled(true);
+    } else {
+      setDisabled(false);
+    }
+  }, [studData, studentProfile]);
 
   if (error) {
     return <div>Error: {error.message}</div>;
   } else if (studData) {
     return (
-      <Container
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-        className='profile-section-container'>
-        <Paper elevation={3} className={classes.contPaper}>
-          <Grid container>
-            <img
-              width='100%'
-              alt='banner img'
-              src={studData.clubBanner ? studData.clubBanner : "/assets/bannerIMG.jpg"}
-              className='profile-banner'
-              key={Date.now()}
-            />
-          </Grid>
-          <Container>
-            <Grid container spacing={7}>
-              <Grid container justify='center'>
-                <Avatar
-                  alt='Club logo'
-                  src={
-                    studData.clubAvatar ? studData.clubAvatar : "/assets/avatar.jpeg"
-                  }
-                  className={classes.avatar}
-                  key={Date.now()}
-                />
-              </Grid>
-              <Grid container style={{ marginTop: "40px" }} justify='center'>
-                <Typography
-                  gutterBottom
-                  variant='h4'
-                  style={{
-                    fontFamily: "Source Sans Pro",
-                    fontWeight: "600",
-                  }}>
-                  Profile Page
-                </Typography>
-              </Grid>
-              <Grid item container xs={12}>
-                <form style={{ width: "100%" }}>
-                  <Grid container spacing={3}>
-                    <Grid item xs={6}>
-                      <Tooltip
-                        title={
-                          <span
-                            style={{
-                              fontSize: "1rem",
-                            }}>
-                            Turning this ON will make your profile public for
-                            the students on CET.
-                          </span>
-                        }
-                        arrow></Tooltip>
+      <NavbarStud>
+        <Container
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+          className='profile-section-container'>
+          <Paper elevation={3} className={classes.contPaper}>
+            <Container>
+              <Grid container spacing={7}>
+                <Grid container style={{ marginTop: "40px" }} justify='center'>
+                  <Typography
+                    gutterBottom
+                    variant='h4'
+                    style={{
+                      fontFamily: "Source Sans Pro",
+                      fontWeight: "600",
+                    }}>
+                    Profile Page
+                  </Typography>
+                </Grid>
+                <Grid item container xs={12}>
+                  <form style={{ width: "100%" }}>
+                    <Grid container spacing={3}>
+                      <Grid item xs={6}>
+                        <TextField
+                          name='name'
+                          className={classes.input}
+                          label='Name'
+                          variant='outlined'
+                          value={studData.name}
+                          onChange={handleProfileChange}
+                        />
+                      </Grid>
+
+                      <Grid item xs={6}>
+                        <TextField
+                          name='branch'
+                          className={classes.input}
+                          label='Branch'
+                          variant='outlined'
+                          value={studData.mobileNumber}
+                          onChange={handleProfileChange}
+                        />
+                      </Grid>
+
+                      <Grid item xs={6}>
+                        <TextField
+                          name='mobileNumber'
+                          className={classes.input}
+                          label='Contact Number'
+                          variant='outlined'
+                          value={studData.mobileNumber}
+                          onChange={handleProfileChange}
+                        />
+                      </Grid>
+                      <Grid item xs={6}>
+                        <TextField
+                          name='github'
+                          className={classes.input}
+                          label='GitHub'
+                          variant='outlined'
+                          value={studData.website}
+                          onChange={handleProfileChange}
+                        />
+                      </Grid>
+                      <Grid item xs={12}>
+                        <TextField
+                          name='bio'
+                          className={classes.input}
+                          label='Bio'
+                          multiline
+                          rows={8}
+                          variant='outlined'
+                          value={studData.bio}
+                          onChange={handleProfileChange}
+                        />
+                      </Grid>
+                      <Grid item xs={12}>
+                        <Button
+                          variant='contained'
+                          color='primary'
+                          // onClick={updateProfile}
+                          disabled={disabled}>
+                          Save Changes
+                        </Button>
+                      </Grid>
                     </Grid>
-                    <Grid item xs={6} style={{ textAlign: "right" }}>
-                      <Tooltip title='Update profile photo'>
-                        {/* <IconButton onClick={() => setDpModal(true)}>
-                          <AccountCircle />
-                        </IconButton> */}
-                      </Tooltip>
-                      <Tooltip title='Update banner'>
-                        {/* <IconButton onClick={() => setBannerModal(true)}>
-                          <AspectRatio />
-                        </IconButton> */}
-                      </Tooltip>
-                    </Grid>
-                  </Grid>
-                  <Grid container spacing={3}>
-                    {/* <Grid item xs={6}>
-                      <TextField
-                        name='name'
-                        className={classes.input}
-                        label='Name'
-                        variant='outlined'
-                        value={studData.name}
-                        onChange={handleProfileChange}
-                      />
-                    </Grid> */}
-                    {/* <Grid item xs={6}>
-                      <TextField
-                        name='type'
-                        className={classes.input}
-                        label='Type'
-                        variant='outlined'
-                        value={studData.type}
-                        onChange={handleProfileChange}
-                      />
-                    </Grid> */}
-                    <Grid item xs={12}>
-                      {/* <TextField
-                        name='bio'
-                        className={classes.input}
-                        label='Description'
-                        multiline
-                        rows={8}
-                        variant='outlined'
-                        value={studData.bio}
-                        onChange={handleProfileChange}
-                      /> */}
-                    </Grid>
-                    {/* <Grid item xs={6}>
-                      <TextField
-                        name='website'
-                        className={classes.input}
-                        label='Club Website Link'
-                        variant='outlined'
-                        value={studData.website}
-                        onChange={handleProfileChange}
-                      />
-                    </Grid> */}
-                    {/* <Grid item xs={6}>
-                      <TextField
-                        name='mobileNumber'
-                        className={classes.input}
-                        label='Club Contact Number'
-                        variant='outlined'
-                        value={studData.mobileNumber}
-                        onChange={handleProfileChange}
-                      />
-                    </Grid> */}
-                    {/* <Grid item xs={12}>
-                      <Button
-                        variant='contained'
-                        color='primary'
-                        onClick={updateProfile}
-                        disabled={disabled}>
-                        Save Changes
-                      </Button>
-                    </Grid> */}
-                  </Grid>
-                </form>
+                  </form>
+                </Grid>
               </Grid>
-            </Grid>
-          </Container>
-        </Paper>
-        {/* <UpdateProfilePhoto
-          open={dpModal}
-          onClose={() => setDpModal(false)}
-          id={studData._id}
-          updateImages={updateImages}
-        /> */}
-        {/* <UpdateBannerPhoto
-          open={bannerModal}
-          onClose={() => setBannerModal(false)}
-          id={studData._id}
-          updateImages={updateImages}
-        /> */}
-        {/* <Snackbar
-          autoHideDuration={4000}
-          onClose={() => setFeatureSuccess(false)}
-          open={featureSuccess}>
-          <Alert
-            severity='success'
-            variant='filled'
-            onClose={() => setFeatureSuccess(false)}>
-            Your profile was successfully made{" "}
-            {studData.featured ? "public" : "private"}!
-          </Alert>
-        </Snackbar> */}
-        {/* <Snackbar
+            </Container>
+          </Paper>
+
+          {/* <Snackbar
           autoHideDuration={4000}
           onClose={() => setProfileSuccess(false)}
           open={profileSuccess}>
@@ -268,7 +185,8 @@ const ClubProfile = () => {
             Your profile was successfully updated!
           </Alert>
         </Snackbar> */}
-      </Container>
+        </Container>
+      </NavbarStud>
     );
   }
   return <div></div>;
