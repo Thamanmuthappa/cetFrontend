06b563df62d12d71d687a05a9d8a911b8cf4c04c
Akshat Gupta <akshatvg23@gmail.com>
2020-12-20 00:01:51 +0530

    home page

--- src/pages/Student/AllDomains/AllDomains.js
+++ src/pages/Student/AllDomains/AllDomains.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { Link } from "react-router-dom";
 import ClubCarousel from "../../../components/Student/ClubCarousel/ClubCarousel";
+import Loading from '../../../pages/Loading';
 import {
   Paper,
   Button,
@@ -8,7 +10,11 @@ import {
   Container,
   Typography,
   Divider,
+  AppBar,
+  Toolbar
 } from "@material-ui/core";
+import MenuIcon from '@material-ui/icons/Menu';
+import '../../../components/Shared/Navbar/Navbar.css';
 
 const AllDomains = () => {
   const [clubs, setClubs] = useState();
@@ -30,8 +36,13 @@ const AllDomains = () => {
   }, []);
 
   const clubsMap = (arr) => {
-    let x = arr.map((club) => <ClubCarousel club={club} />);
-    return x;
+    if (arr.length != 0) {
+      let x = arr.map((club) => <ClubCarousel club={club} />);
+      return x;
+    }
+    else {
+      return "No public organisation in this domain available right now 😞";
+    }
   };
 
   if (err) {
@@ -65,98 +76,134 @@ const AllDomains = () => {
         otherClubs.push(clubs.clubs[x]);
       }
     }
-
-    console.log(techClubs);
     return (
-      <Container>
-        <div style={{ display: "flex" }}>
-          <Typography
-            gutterBottom
-            variant='h2'
-            style={{
-              fontFamily: "Source Sans Pro",
-              fontWeight: "600",
-            }}>
-            Featured Clubs
+      <div>
+        <AppBar
+          className="navbar"
+          style={{
+            backgroundColor: "#fdf9f9",
+            zIndex: "1400",
+
+            position: "relative",
+          }}
+          elevation={2}
+        >
+          <Toolbar>
+            <Link to={`/student/dashboard`}>
+              <img
+                src="/assets/Group2.png"
+                alt="logo"
+                className="nav-img"
+              />
+            </Link>
+            <Typography
+              variant="h5"
+              className="nav-brand"
+              style={{ flex: 1 }}
+            >
+              Dashboard
+				</Typography>
+            <Button className="logout-btn-nav" href="/">
+              LogOut
+				</Button>
+          </Toolbar>
+        </AppBar>
+        <Container style={{ marginBottom: "40px" }}>
+          <div style={{ display: "flex", marginTop: "40px" }}>
+            <Typography
+              gutterBottom
+              variant='h3'
+              style={{
+                fontFamily: "Source Sans Pro",
+                fontWeight: "600",
+              }}>
+              Featured Clubs
           </Typography>
-        </div>
-        <Divider />
-        <Grid container style={{ marginTop: "20px" }}>
-          <Typography
-            gutterBottom
-            variant='h3'
-            style={{
-              fontFamily: "Source Sans Pro",
-              fontWeight: "600",
-            }}>
-            Technical
+          </div>
+          <Divider />
+          <Grid container style={{ marginTop: "40px" }}>
+            <Typography
+              gutterBottom
+              variant='h4'
+              style={{
+                fontFamily: "Source Sans Pro",
+                fontWeight: "600",
+              }}>
+              Technical
           </Typography>
+          </Grid>
+          <Grid container style={{ marginTop: "40px", marginBottom: "40px" }}>{clubsMap(techClubs)}</Grid>
+          <Grid container style={{ marginTop: "40px" }}>
+            <Typography
+              gutterBottom
+              variant='h4'
+              style={{
+                fontFamily: "Source Sans Pro",
+                fontWeight: "600",
+              }}>
+              Arts and Culture
+          </Typography>
+          </Grid>
+          <Grid container style={{ marginTop: "40px", marginBottom: "40px" }}>{clubsMap(artsClubs)}</Grid>
+          <Grid container style={{ marginTop: "40px" }}>
+            <Typography
+              gutterBottom
+              variant='h4'
+              style={{
+                fontFamily: "Source Sans Pro",
+                fontWeight: "600",
+              }}>
+              Social Outreach
+          </Typography>
+          </Grid>
+          <Grid container style={{ marginTop: "40px", marginBottom: "40px" }}>{clubsMap(socialClubs)}</Grid>
+          <Grid container style={{ marginTop: "40px" }}>
+            <Typography
+              gutterBottom
+              variant='h4'
+              style={{
+                fontFamily: "Source Sans Pro",
+                fontWeight: "600",
+              }}>
+              Health and Wellness
+          </Typography>
+          </Grid>
+          <Grid container style={{ marginTop: "40px", marginBottom: "40px" }}>{clubsMap(healthClubs)}</Grid>
+          <Grid container style={{ marginTop: "40px" }}>
+            <Typography
+              gutterBottom
+              variant='h4'
+              style={{
+                fontFamily: "Source Sans Pro",
+                fontWeight: "600",
+              }}>
+              Literature
+          </Typography>
+          </Grid>
+          <Grid container style={{ marginTop: "40px", marginBottom: "40px" }}>{clubsMap(litClubs)}</Grid>
+          <Grid container style={{ marginTop: "40px" }}>
+            <Typography
+              gutterBottom
+              variant='h4'
+              style={{
+                fontFamily: "Source Sans Pro",
+                fontWeight: "600",
+              }}>
+              Other
+          </Typography>
+          </Grid>
+          <Grid container style={{ marginTop: "40px", marginBottom: "40px" }}>{clubsMap(otherClubs)}</Grid>
+        </Container>
+        <Grid style={{ backgroundColor: "#FCF9F9", color: "#2C2D2D", padding: "25px" }}>
+          <center>
+            <a href="https://www.codechefvit.com" target="_blank"><img src="/assets/blacklogo.png" alt="CodeChef-VIT" width="150px" align="center" /></a>
+          </center>
+          <p style={{ textAlign: "center" }}>Imagined, Designed and Developed by <a href="https://www.codechefvit.com" style={{ color: "#E31E43" }} target="_blank">CodeChef-VIT</a></p>
         </Grid>
-        <Grid container>{clubsMap(techClubs)}</Grid>
-        <Grid container style={{ marginTop: "20px" }}>
-          <Typography
-            gutterBottom
-            variant='h3'
-            style={{
-              fontFamily: "Source Sans Pro",
-              fontWeight: "600",
-            }}>
-            Arts and Culture
-          </Typography>
-        </Grid>
-        <Grid container>{clubsMap(artsClubs)}</Grid>
-        <Grid container style={{ marginTop: "20px" }}>
-          <Typography
-            gutterBottom
-            variant='h3'
-            style={{
-              fontFamily: "Source Sans Pro",
-              fontWeight: "600",
-            }}>
-            Social Outreach
-          </Typography>
-        </Grid>
-        <Grid container>{clubsMap(socialClubs)}</Grid>
-        <Grid container style={{ marginTop: "20px" }}>
-          <Typography
-            gutterBottom
-            variant='h3'
-            style={{
-              fontFamily: "Source Sans Pro",
-              fontWeight: "600",
-            }}>
-            Health and Wellness
-          </Typography>
-        </Grid>
-        <Grid container>{clubsMap(healthClubs)}</Grid>
-        <Grid container style={{ marginTop: "20px" }}>
-          <Typography
-            gutterBottom
-            variant='h3'
-            style={{
-              fontFamily: "Source Sans Pro",
-              fontWeight: "600",
-            }}>
-            literature
-          </Typography>
-        </Grid>
-        <Grid container>{clubsMap(litClubs)}</Grid>
-        <Grid container style={{ marginTop: "20px" }}>
-          <Typography
-            gutterBottom
-            variant='h3'
-            style={{
-              fontFamily: "Source Sans Pro",
-              fontWeight: "600",
-            }}>
-            Other
-          </Typography>
-        </Grid>
-        <Grid container>{clubsMap(otherClubs)}</Grid>
-      </Container>
+      </div>
     );
   } else {
-    return <div>loading</div>;
+    return <Loading />;
   }
 };
 
