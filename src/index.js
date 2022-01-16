87f6d74c2194dd6999f722ac086b24c0b9343739
Vinayak Gupta <guptavinayakg@gmail.com>
2022-01-16 18:01:31 +0530

    nice..

--- src/index.js
+++ src/index.js
@@ -2,33 +2,33 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { createTheme, ThemeProvider } from '@material-ui/core/styles';
+import { createTheme, ThemeProvider } from "@material-ui/core/styles";
 
 const theme = createTheme({
-	typography: {
-		fontFamily: "Source Sans Pro",
-	},
-	palette: {
-		primary: {
-			main: "#1799E1",
-		},
-		secondary: {
-			main: "#3E62DF",
-		},
-		whitetext: {
-			main: "#fafafa",
-		},
-		pureWhite: {
-			main: "#FFFFFF",
-		},
-	},
+  typography: {
+    fontFamily: "Source Sans Pro",
+  },
+  palette: {
+    primary: {
+      main: "#1799E1",
+    },
+    secondary: {
+      main: "#3E62DF",
+    },
+    whitetext: {
+      main: "#fafafa",
+    },
+    pureWhite: {
+      main: "#FFFFFF",
+    },
+  },
 });
 
 ReactDOM.render(
-	<React.StrictMode>
-		<ThemeProvider theme={theme}>
-			<App />
-		</ThemeProvider>
-	</React.StrictMode>,
-	document.getElementById("root")
+  // <React.StrictMode>
+  <ThemeProvider theme={theme}>
+    <App />
+  </ThemeProvider>,
+  // </React.StrictMode>,
+  document.getElementById("root")
 );
--- src/pages/Student/TestScreen/TestScreen.jsx
+++ src/pages/Student/TestScreen/TestScreen.jsx
@@ -5,23 +5,25 @@ import {
   DialogActions,
   DialogTitle,
   Divider,
-  Fab,
   Grid,
   Tooltip,
   Typography,
 } from "@material-ui/core";
-import { Done } from "@material-ui/icons";
 import Axios from "axios";
 import React, { useEffect, useState } from "react";
+import Countdown from "react-countdown";
+import { useHistory } from "react-router-dom";
+import CountDownModal from "../../../components/CountDownModal/CountDownModal";
 import StudentNavbar from "../../../components/Student/StudentNavbar/StudentNavbar";
 import TestQuestionDisplay from "../../../components/Student/TestQuestionDisplay/TestQuestionDisplay";
 import Loading from "../../Loading";
 import { dummyTest } from "./dummyTest";
 import "./TestScreen.css";
-import Countdown from "react-countdown";
-import { useHistory } from "react-router-dom";
-import {fullscreenWindow, closeFullscreen, fullScreenListeners, removeFullScreenListeners} from "./utils";
-import CountDownModal from "../../../components/CountDownModal/CountDownModal";
+import {
+  fullScreenListeners,
+  fullscreenWindow,
+  removeFullScreenListeners,
+} from "./utils";
 
 const TestScreen = (props) => {
   const [testDetails, setTestDetails] = useState(dummyTest);
@@ -33,49 +35,47 @@ const TestScreen = (props) => {
   const [startedAt, setStartedAt] = useState(Date.now() * 100000);
   const history = useHistory();
   const [countdownModal, setCountdownModal] = useState(false);
+  const [violated, setViolated] = useState(false);
 
   const minsToMilli = (mins) => {
     return mins * 60 * 1000;
   };
 
-
   const sendRequest = async (final) => {
     const url = `${process.env.REACT_APP_BACKEND_URL}/test/domain/submit`;
     const token = localStorage.getItem("studentAuthToken");
     console.log("Sending request");
+
     await Axios.post(url, final, {
       headers: {
         Authorization: `Bearer ${token}`,
       },
-    }).then((res) => {
-      setLoading(false);
-      setSubmitted(true);
-      console.log("Submitting Test");
     })
+      .then((res) => {
+        setLoading(false);
+        setSubmitted(true);
+        console.log("Submitting Test");
+      })
       .catch((err) => {
         setLoading(false);
         setError(true);
         console.log(err);
       });
-  }
+  };
 
-
-
-  const submitTest = async () => {
+  const submitTest = () => {
     setLoading(true);
-    console.log("Submit test Called")
-
+    console.log("Submit test Called");
     const final = JSON.parse(JSON.stringify(answers));
-    final.timeTaken = (Date.now() - startedAt) / 60000
-
-    await sendRequest(final);
+    final.timeTaken = (Date.now() - startedAt) / 1000;
+    sendRequest(final);
   };
 
   const handleTimeout = () => {
     console.log("Time Out");
     submitTest();
   };
-  
+
   const createAnsObject = (testDetails) => {
     const obj = {};
     obj.domainId = testDetails.domainDetails._id;
@@ -96,6 +96,35 @@ const TestScreen = (props) => {
     return obj;
   };
 
+  const handleTabChange = () => {
+    setCountdownModal(true);
+    // console.log(document.visibilityState);
+  };
+
+  const handleFullScreenExit = () => {
+    if (
+      !document.webkitIsFullScreen &&
+      !document.mozFullScreen &&
+      !document.msFullscreenElement
+    ) {
+      handleTabChange();
+    }
+  };
+  const handleModalClose = () => {
+    setCountdownModal(false);
+    fullscreenWindow();
+  };
+
+  const handleTestViolation = () => {
+    setViolated(true);
+    console.log("test violated");
+    setCountdownModal(false);
+  };
+
+  useEffect(() => {
+    if (violated) submitTest();
+  }, [violated]);
+
   useEffect(() => {
     try {
       const details = props.location.state.details;
@@ -114,42 +143,13 @@ const TestScreen = (props) => {
 
     setLoading(false);
   }, [props.location.state.startedAt, props.location.state.details]);
-  
-    const handleTabChange = () => {
-		setCountdownModal(true);
-		// console.log(document.visibilityState);
-	};
-
-	const handleFullScreenExit = () => {
-		if (
-			!document.webkitIsFullScreen &&
-			!document.mozFullScreen &&
-			!document.msFullscreenElement
-		) {
-			handleTabChange();
-		}
-	};
-  const handleModalClose = () => {
-		setCountdownModal(false);
-		fullscreenWindow();
-	};
-  const handleTestViolation = () => {
-		// setViolated(true);
-    // setConfirmSubmit(false);
-    console.log("test violated");
-    if(!setConfirmSubmit(true)){
-		submitTest();
-    }
-    setCountdownModal(false);
-    // setSubmitted(true);
-	};
 
   useEffect(() => {
     fullscreenWindow();
     fullScreenListeners(handleFullScreenExit);
     return () => {
       removeFullScreenListeners();
-    }
+    };
   }, []);
 
   if (loading) {
@@ -159,100 +159,126 @@ const TestScreen = (props) => {
   }
 
   return (
-    <div className='test-page' style={{ paddingBottom: "40px", background: "#081220" }}>
-      <StudentNavbar location='Attempt test' />
+    <div
+      className="test-page"
+      style={{ paddingBottom: "40px", background: "#081220" }}
+    >
+      <StudentNavbar location="Attempt test" />
       <div className="cont-colo">
-      <Container className='test-container' style={{ paddingBottom: "40px", color: "#081220"}}>
-        <div className='student-test-details'>
-          <Grid container spacing={3}>
-            <Grid item xs={12} sm={6} style={{ textAlign: "center", color: "#fff" }}>
-              <Typography variant='h4' className='light-text' style={{color: "#fff"}}>
-                Club Name: <strong>{testDetails.clubDetails.name}</strong>
-              </Typography>
-            </Grid>
-            <Grid item xs={12} sm={6} style={{ textAlign: "center", color: "#fff"}}>
-              <Typography variant='h4' className='light-text' style={{color: "#fff"}}>
-                Domain:{" "}
-                <strong> {testDetails.domainDetails.domainName} </strong>
-              </Typography>
-            </Grid>
-            <Divider
-              style={{
-                height: "1px",
-                width: "100%",
-                backgroundColor: "#f5f5f540",
-              }}
-            />
-            <Grid item xs={12} className='test-timer-grid'>
-              <div className='test-time-remaining'>
-                <Typography variant='h4' color='primary'>
-                  <strong>
-                    <Countdown
-                      daysInHours
-                      date={
-                        startedAt +
-                        minsToMilli(testDetails.domainDetails.domainDuration)
-                      }
-                      onComplete={handleTimeout}>
-                      <span>Time Up!</span>
-                    </Countdown>
-                    {/* {timeRemaining} minutes remaining!{" "}
-										TODO: MAKE DYNAMIC */}{" "}
-                  </strong>
+        <Container
+          className="test-container"
+          style={{ paddingBottom: "40px", color: "#081220" }}
+        >
+          <div className="student-test-details">
+            <Grid container spacing={3}>
+              <Grid
+                item
+                xs={12}
+                sm={6}
+                style={{ textAlign: "center", color: "#fff" }}
+              >
+                <Typography
+                  variant="h4"
+                  className="light-text"
+                  style={{ color: "#fff" }}
+                >
+                  Club Name: <strong>{testDetails.clubDetails.name}</strong>
                 </Typography>
-              </div>
+              </Grid>
+              <Grid
+                item
+                xs={12}
+                sm={6}
+                style={{ textAlign: "center", color: "#fff" }}
+              >
+                <Typography
+                  variant="h4"
+                  className="light-text"
+                  style={{ color: "#fff" }}
+                >
+                  Domain:{" "}
+                  <strong> {testDetails.domainDetails.domainName} </strong>
+                </Typography>
+              </Grid>
+              <Divider
+                style={{
+                  height: "1px",
+                  width: "100%",
+                  backgroundColor: "#f5f5f540",
+                }}
+              />
+              <Grid item xs={12} className="test-timer-grid">
+                <div className="test-time-remaining">
+                  <Typography variant="h4" color="primary">
+                    <strong>
+                      <Countdown
+                        daysInHours
+                        date={
+                          startedAt +
+                          minsToMilli(testDetails.domainDetails.domainDuration)
+                        }
+                        onComplete={handleTimeout}
+                      >
+                        <span>Time Up!</span>
+                      </Countdown>
+                      {/* {timeRemaining} minutes remaining!{" "}
+										TODO: MAKE DYNAMIC */}{" "}
+                    </strong>
+                  </Typography>
+                </div>
+              </Grid>
+              <Divider style={{ width: "100%", background: "#f5f5f540" }} />
             </Grid>
-            <Divider style={{ width: "100%" , background: "#f5f5f540"}} />
-          </Grid>
-        </div>
-        <div className='student-questions-display'>
-          {testDetails.questions.map((question, i) => (
-            <TestQuestionDisplay
-              question={question}
-              index={i}
-              answers={answers}
-              setAnswers={setAnswers}
-              key={i}
-            />
-          ))}
-        </div>
-      </Container>
+          </div>
+          <div className="student-questions-display">
+            {testDetails.questions.map((question, i) => (
+              <TestQuestionDisplay
+                question={question}
+                index={i}
+                answers={answers}
+                setAnswers={setAnswers}
+                key={i}
+              />
+            ))}
+          </div>
+        </Container>
       </div>
       <div className="submit-test-button">
-      <Tooltip title='Submit Test'>
-        <Button
-          color='primary'
-          variant='contained'
-          className='submittestbutton'
-          onClick={() => {
-          setConfirmSubmit(true)
-            console.log("Set Confirm Submit");
-        }
-          }>
-          Submit Test
-        </Button>
-        
-        {/* <Fab
+        <Tooltip title="Submit Test">
+          <Button
+            color="primary"
+            variant="contained"
+            className="submittestbutton"
+            onClick={() => {
+              setConfirmSubmit(true);
+              console.log("Set Confirm Submit");
+            }}
+          >
+            Submit Test
+          </Button>
+
+          {/* <Fab
           color='primary'
           aria-label='submit-test'
           className='submit-fab'
           onClick={() => setConfirmSubmit(true)}>
           <Done />
         </Fab> */}
-      </Tooltip>
+        </Tooltip>
       </div>
       <Dialog
         open={confirmSubmit}
         onClose={() => setConfirmSubmit(false)}
-        fullWidth>
-        <DialogTitle style={{color: "white", background:"#252D3A"}}>
+        fullWidth
+      >
+        <DialogTitle style={{ color: "white", background: "#252D3A" }}>
           Are you sure you want to submit this domain test?
         </DialogTitle>
-        <DialogActions style={{color: "white", background:"#252D3A"}}>
-          <Button variant='contained' onClick={() => setConfirmSubmit(false)}>
+        <DialogActions style={{ color: "white", background: "#252D3A" }}>
+          <Button variant="contained" onClick={() => setConfirmSubmit(false)}>
             Cancel
           </Button>
-          <Button variant='contained' color='primary' onClick={submitTest}>
+          <Button variant="contained" color="primary" onClick={submitTest}>
             Submit
           </Button>
         </DialogActions>
@@ -267,12 +293,15 @@ const TestScreen = (props) => {
             },
           });
         }}
-        fullWidth>
-        <DialogTitle style={{color: "white", background:"#252D3A"}}>Test Submitted Successfully</DialogTitle>
-        <DialogActions style={{color: "white", background:"#252D3A"}}>
+        fullWidth
+      >
+        <DialogTitle style={{ color: "white", background: "#252D3A" }}>
+          Test Submitted Successfully
+        </DialogTitle>
+        <DialogActions style={{ color: "white", background: "#252D3A" }}>
           <Button
-            variant='contained'
-            color='primary'
+            variant="contained"
+            color="primary"
             onClick={() => {
               history.replace({
                 pathname: `/student/test/domains/${answers.testId}`,
@@ -280,16 +309,17 @@ const TestScreen = (props) => {
                   details: props.location.state.testDetails,
                 },
               });
-            }}>
+            }}
+          >
             Continue to another test
           </Button>
         </DialogActions>
       </Dialog>
       <CountDownModal
-				open={countdownModal}
-				onClose={handleModalClose}
-				onComplete={handleTestViolation}
-			/>
+        open={countdownModal}
+        onClose={handleModalClose}
+        onComplete={handleTestViolation}
+      />
     </div>
   );
 };
